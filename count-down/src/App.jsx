e4f449fd8b317be020f2dc3c64d339c4651d9367
i-tea <i-tea@email.com>
2017-08-06 21:49:33 +0200

    countdown structure and basic styles

--- count-down/src/App.jsx
+++ count-down/src/App.jsx
@@ -1,15 +1,21 @@
 import React, { Component } from 'react'; // React { Component } cause we also wanna use components
+import './App.css';
 
 class App extends Component {
+  //JSX make different keyword to make classname, cause class is reserved, have to use className
   render() {
     return(
-      <div>
-        <div>Countdown to December 24, 2017</div>
+      <div className="App">
+        <div className="App-title">Countdown to December 24, 2017</div>
         <div>
-          <div>14 days</div>
-          <div>30 hours</div>
-          <div>15 minutes</div>
-          <div>20 seconds</div>
+          <div className="Clock-days">14 days</div>
+          <div className="Clock-hours">30 hours</div>
+          <div className="Clock-minutes">15 minutes</div>
+          <div className="Clock-seconds">20 seconds</div>
+        </div>
+        <div>
+          <input placeholder='new date' />
+          <button>Submit</button> 
         </div>
       </div>
     )
